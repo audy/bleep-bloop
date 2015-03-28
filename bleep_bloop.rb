2a75273850b315cf13953dab5cc360a4a44bf9cf
Austin Davis-Richardson <harekrishna@gmail.com>
2015-03-28 13:45:44 -0400

    changed up the bloops

--- bleep_bloop.rb
+++ bleep_bloop.rb
@@ -2,25 +2,20 @@
 
 require 'bloops'
 
-bleep = Bloops.new
 
-bleep.tempo = [60, 120, 240].sample
-l = %w{a b c d e f g}
-song = (5..10).to_a.sample.times.map { l.sample }.map { |x| "#{x}#{(3..5).to_a.sample}" }.join(' ')
+def make_instrument!
+  bleep = Bloops.new
+  bleep.tempo = 120
 
-instrument = bleep.sound [Bloops::SINE, Bloops::SQUARE].sample
+  instrument = bleep.sound Bloops::SINE
 
-instrument.punch = rand*0.25 + 0.5
-instrument.sustain = 1
-instrument.decay = rand*0.25 + 0.5
-instrument.arp = rand*0.25 + 0.5
-instrument.aspeed = rand*0.25 + 0.5
-#instrument.repeat = rand*0.25 + 0.5 # makes everything the same tone
-instrument.phase = rand*0.25 + 0.5
-instrument.psweep = rand*0.25 + 0.5
+  [bleep, instrument]
+end
 
-bleep.tune instrument, song
-
-bleep.play
-
-sleep 1 while !bleep.stopped?
+while song = gets.strip
+  bleep, instrument = make_instrument!
+  puts "playing: #{song}"
+  bleep.tune(instrument, song)
+  bleep.play
+  sleep 1 while !bleep.stopped?
+end
